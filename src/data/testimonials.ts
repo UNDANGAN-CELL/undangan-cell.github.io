b35389efdbf8c8efb2140158c5888514296c1b6f
UNDANGAN-CELL <006akuntektok@gmail.com>
2026-09-19 21:31:08 +0700

    gil

--- src/data/testimonials.ts
+++
@@ -1,6 +0,0 @@
-import { Testimonial } from '../types';
-
-// Belum ada data testimoni pelanggan yang dapat diverifikasi.
-// Isi array ini hanya setelah nama, komentar, dan detail pelanggan memang
-// telah mendapatkan izin untuk dipublikasikan.
-export const TESTIMONIALS: Testimonial[] = [];
